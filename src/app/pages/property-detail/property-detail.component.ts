71d08846919b1889ddb330fce14883db3dadb2a3
luishdsg <65797541+luishdsg@users.noreply.github.com>
2022-12-26 15:13:06 -0300

    updates

--- src/app/pages/property-detail/property-detail.component.ts
+++ src/app/pages/property-detail/property-detail.component.ts
@@ -13,7 +13,7 @@ export class PropertyDetailComponent implements OnInit {
 
   form: FormGroup;
   formproperty: FormGroup;
-  
+
   changeSubscription: Subscription;
 
   iconlikeheart = false;
@@ -90,7 +90,7 @@ export class PropertyDetailComponent implements OnInit {
       this.tourvirtual = true;
     }
   }
-  goExpress(){
+  goExpress() {
     this.router.navigate(['logged/express']);
   }
   nextScheduling(value: string) {
@@ -102,7 +102,7 @@ export class PropertyDetailComponent implements OnInit {
       this.step3scheduling = true;
     } else if (value === 'close') {
       this.modalscheduling = false;
-      this.step1scheduling = false; 
+      this.step1scheduling = false;
       this.step2scheduling = false;
       this.step3scheduling = false;
     } else if (value === 'viewvisits') {
@@ -113,12 +113,12 @@ export class PropertyDetailComponent implements OnInit {
       setTimeout(() => {
         this.router.navigate(['logged/visits']);
       }, 100);
-    } 
+    }
     // undefined
     else if (value === 'modal-logged') {
       this.modallogin = true;
-    }else if(value === 'modal-scheduling'){
-      this.modalscheduling = true; 
+    } else if (value === 'modal-scheduling') {
+      this.modalscheduling = true;
       this.step1scheduling = true;
     }
   }
