8e345928e04ab0d327633e96aef32df3fcd623ff
Alan Oliveira <alandivan@gmail.com>
2023-01-24 15:27:40 -0300

    updated login

--- src/app/auth/modal-code/modal-code.component.ts
+++ src/app/auth/modal-code/modal-code.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
 import { AuthetincatedUserDto } from 'src/app/dtos/authenticated-user.dto';
 import { AuthenticateCodeConfirmationRequestDto } from 'src/app/dtos/authentication-code-confirmation.dtos';
@@ -22,12 +23,18 @@ export class ModalCodeComponent implements OnInit {
   notsendcodemsg = false;
   request: any = AuthenticateCodeConfirmationRequestDto;
 
+  codigo1: string;
+  codigo2: string;
+  codigo3: string;
+  codigo4: string;
+
   constructor(
     private router: Router,
     private toastrService: ToastrService,
     private authenticationService: AuthenticationService,
     private formBuilder: FormBuilder,
-    private userService: UserService
+    private userService: UserService,
+    private modalService: NgbModal
   ) {
     this.form = this.formBuilder.group({
       code1: ['', [Validators.required]],
@@ -38,9 +45,8 @@ export class ModalCodeComponent implements OnInit {
   }
   ngOnInit(): void {
     this.phone = localStorage.getItem('phone');
-    console.log(this.phone);
     if (this.phone === null) {
-      this.router.navigate(['auth/sign-in']);
+      this.modalService.dismissAll()
     }
   }
   notSendCode(value: string) {
@@ -51,43 +57,57 @@ export class ModalCodeComponent implements OnInit {
     }
   }
   async confirm() {
-    //   this.spinnerload = true;
-    //   this.continued = false;
-    //   this.request = {
-    //     code: `${this.form.controls['code1'].value}${this.form.controls['code2'].value}${this.form.controls['code3'].value}${this.form.controls['code4'].value}`,
-    //     phone: this.phone
-    //   }
-    //   this.authenticationService.authenticateCodeConfirmation(this.request).subscribe(
-    //     async success => {
-    //       localStorage.removeItem('phone');
-    //       this.authenticationService.setAuthenticatedUser(
-    //         new AuthetincatedUserDto(success.userId, success.phone, success.token, success.profileId, success.apiFunctionsId),
-    //         );
+      this.spinnerload = true;
+      this.continued = false;
+      this.request = {
+        code: `${this.form.controls['code1'].value}${this.form.controls['code2'].value}${this.form.controls['code3'].value}${this.form.controls['code4'].value}`,
+        phone: this.phone
+      }
+      this.authenticationService.authenticateCodeConfirmation(this.request).subscribe(
+        async success => {
+          localStorage.removeItem('phone');
+          this.authenticationService.setAuthenticatedUser(
+            new AuthetincatedUserDto(success.userId, success.phone, success.token, success.profileId, success.apiFunctionsId),
+            );
 
-    //         this.userService.getUser().subscribe(
-    //           success => {
-    //             let user = JSON.stringify(success);
-    //             localStorage.setItem('userDto', user);
-    //             this.router.navigate(['home']);
-    //         },
-    //         error => {
-    //           console.error(error)
-    //         }
-    //       )
-    //     },
-    //     async error => {
-    //       this.toastrService.error('Erro ao cadastrar ', 'Toastr fun!', { progressBar: true });
-    //       console.error(error, this.request)
-    //       this.spinnerload = false;
-    //       this.continued = true;
-    //       this.form.setValue(this.form.controls['code1'].value = ''});
-    //     }
-    //   )
+            this.userService.getUser().subscribe(
+              success => {
+                let user = JSON.stringify(success);
+                localStorage.setItem('userDto', user);
+                this.router.navigate(['/']);
+                this.modalService.dismissAll()
+            },
+            error => {
+              console.error(error)
+            }
+          )
+        },
+        async error => {
+          this.toastrService.error('Erro ao confirmar codigo', '', { progressBar: true });
+          this.spinnerload = false;
+          this.continued = true;
+        }
+      )
   }
+
   onDigitInput(event: any, item) {
     let digito = event.target.value
-    if(item === 'code1') {
-      console.log(digito)
+    if (item === 'code1') {
+      this.codigo1 = digito;
+      var nextInput = document.getElementById('code2');
+      nextInput.focus();
+    } else if (item === 'code2') {
+      this.codigo2 = digito;
+      var nextInput = document.getElementById('code3');
+      nextInput.focus();
+    } else if (item === 'code3') {
+      this.codigo3 = digito;
+      var nextInput = document.getElementById('code4');
+      nextInput.focus();
+    } else if (item === 'code4') {
+      this.codigo4 = digito;
+      var nextInput = document.getElementById('btn-send-code');
+      nextInput.focus();
     }
   }
 
