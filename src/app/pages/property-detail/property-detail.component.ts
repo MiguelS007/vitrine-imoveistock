1488a282d25d1cbdc54512f1cb70a9771b81b3a1
Alan Oliveira <alandivan@gmail.com>
2023-01-26 14:44:47 -0300

    updated

--- src/app/pages/property-detail/property-detail.component.ts
+++ src/app/pages/property-detail/property-detail.component.ts
@@ -84,6 +84,8 @@ export class PropertyDetailComponent implements OnInit {
 
     this.response = this.route.snapshot.data['resolve'];
 
+    console.log(this.response)
+
     // this.searchService.listByAdvertizer().subscribe(
     //   success => {
     //     this.response = success;
