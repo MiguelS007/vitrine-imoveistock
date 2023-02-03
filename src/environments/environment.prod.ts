68b60f3ecc51fe0688d08541dcbe33d450b92246
Danilo de Falco <57508216+danilodefalco@users.noreply.github.com>
2023-02-03 11:59:03 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  payloadKey: '57b0e476-142c-4762-8547-140b65cb1e9c',
+  apis: {
+    // imoveistock: 'https://market-inkluziva.mustblockchain.com/imoveistock/',
+    imoveistock: 'http://localhost:5000/',
+  },
+
+};
 };
