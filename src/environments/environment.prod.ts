753ae5a987620a066f3c9381362d956e06005975
Danilo de Falco <57508216+danilodefalco@users.noreply.github.com>
2023-03-06 18:47:38 -0300

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   payloadKey: '57b0e476-142c-4762-8547-140b65cb1e9c',
   apis: {
     // imoveistock: 'https://market-inkluziva.mustblockchain.com/imoveistock/',
-    imoveistock: 'https://market-inkluziva.mustblockchain.com/imoveistock/',
+    imoveistock: 'https://imoveistockapi.tgtdigital.io/',
   },
   google: {
     apiKey: 'AIzaSyBzwijvQ13DZALYBwC-uKdWWELaf0r9Xzs',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   payloadKey: '57b0e476-142c-4762-8547-140b65cb1e9c',
   apis: {
-    imoveistock: 'http://localhost:5000/',
+    imoveistock: 'https://imoveistockapi.tgtdigital.io/',
     // imoveistock: 'http://ec2-100-26-202-160.compute-1.amazonaws.com:5000/',
   },
   google: {
