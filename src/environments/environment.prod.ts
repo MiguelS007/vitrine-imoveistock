9d97ddfd76364ad7d0ec5f1ec3ef4a424b7bbed9
iagofeelix <93677121+iagofeelix@users.noreply.github.com>
2023-05-15 13:06:21 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   payloadKey: '57b0e476-142c-4762-8547-140b65cb1e9c',
   apis: {
     /*imoveistock: 'https://marketplace-inkluziva.tgtdigital.io/homolog-imoveistock/', */
-     imoveistock: 'https://imoveistockapi.tgtdigital.io/',
+     imoveistock: 'https://imoveistock-prod.tgtdigital.io/',
   },
     google: {
     apiKey: 'AIzaSyBzwijvQ13DZALYBwC-uKdWWELaf0r9Xzs',
