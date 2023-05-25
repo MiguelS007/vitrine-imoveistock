057cacac6be38822ea82d373ac53585a76f6045e
Pedro Fiuza <pedrofiuza1@gmail.com>
2023-05-25 10:57:45 -0300

    create count district

--- src/app/pages/home/home-gallery/home-gallery.component.ts
+++ src/app/pages/home/home-gallery/home-gallery.component.ts
@@ -1,29 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import {AnnouncementService} from '../../../service/announcement.service';
+import { AnnouncementService } from '../../../service/announcement.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-gallery',
   templateUrl: './home-gallery.component.html',
-  styleUrls: ['./home-gallery.component.scss']
+  styleUrls: ['./home-gallery.component.scss'],
 })
 export class HomeGalleryComponent implements OnInit {
+  response: { district: string; count: number }[] = [];
 
-  constructor(private announcementService:AnnouncementService,private router: Router) { }
+  constructor(
+    private announcementService: AnnouncementService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.announcementService.countDistrict().subscribe({
+      next: (data) => (this.response = data),
+      error: (err) => console.log(err),
+    });
   }
 
-  searchByDistrict(district: string){
+  searchByDistrict(district: string) {
+    let requestList = {
+      cityAddress: 'Curitiba',
+      ufAddress: 'PR',
+    };
+
     this.announcementService.listByDistrict(district).subscribe({
       next: (data) => {
         localStorage.setItem('resultSearch', JSON.stringify(data));
+        localStorage.setItem('filtro', JSON.stringify(requestList));
         this.router.navigate(['/search']);
       },
       error: (err) => {
         console.log(err);
-      }
-    })
+      },
+    });
   }
 
+  getCountByDistrict(district: string) {
+    return (
+      this.response.find(
+        (x) => x.district.toLowerCase() === district.toLowerCase()
+      )?.count || 0
+    );
+  }
 }
