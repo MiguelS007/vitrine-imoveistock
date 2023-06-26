45d288b58ff37e6cee866f6623e31b354e5df11d
Pedro Fiuza <pedrofiuza1@gmail.com>
2023-06-26 12:36:22 -0300

    update

--- src/app/service/schedule.service.ts
+++ src/app/service/schedule.service.ts
@@ -13,57 +13,89 @@ import { BaseService } from './base.service';
 import { AnnouncementVisitGetResponseDto } from '../dtos/announcement-visit-get-response.dto';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class ScheduleService extends BaseService {
+  url: string = `${environment.apis.imoveistock}`;
 
-    url: string = `${environment.apis.imoveistock}`;
+  constructor(private httpClient: HttpClient) {
+    super();
+  }
 
-    constructor(
-        private httpClient: HttpClient,
-    ) {
-        super();
-    }
+  registerSchedule(
+    _id: string,
+    dto: ScheduleRegisterRequestDto
+  ): Observable<ScheduleRegisterResponseDto> {
+    return this.httpClient
+      .post(
+        `${this.url}app/announcement-visit/register/${_id}`,
+        dto,
+        this.authorizedHeader()
+      )
+      .pipe(map(this.extractData), catchError(this.serviceError));
+  }
 
-    registerSchedule(_id: string, dto: ScheduleRegisterRequestDto): Observable<ScheduleRegisterResponseDto> {
-        return this.httpClient
-            .post(`${this.url}app/announcement-visit/register/${_id}`, dto, this.authorizedHeader())
-            .pipe(map(this.extractData), catchError(this.serviceError));
-    }
+  reschedule(
+    _id: string,
+    dto: VisitRescheduleRegisterDto
+  ): Observable<ScheduleRegisterResponseDto> {
+    return this.httpClient
+      .patch(
+        `${this.url}app/announcement-visit/reschedule-visit/id/${_id}`,
+        dto,
+        this.authorizedHeader()
+      )
+      .pipe(map(this.extractData), catchError(this.serviceError));
+  }
 
-    reschedule(_id: string, dto: VisitRescheduleRegisterDto): Observable<ScheduleRegisterResponseDto> {
-        return this.httpClient
-            .patch(`${this.url}app/announcement-visit/reschedule-visit/id/${_id}`, dto, this.authorizedHeader())
-            .pipe(map(this.extractData), catchError(this.serviceError));
-    }
+  getListVisists(): Observable<AnnouncementVisitGetResponseDto[]> {
+    return this.httpClient
+      .get(`${this.url}app/announcement-visit/list`, this.authorizedHeader())
+      .pipe(map(this.extractData), catchError(this.serviceError));
+  }
 
-    getListVisists(): Observable<AnnouncementVisitGetResponseDto[]> {
-        return this.httpClient
-            .get(`${this.url}app/announcement-visit/list`, this.authorizedHeader())
-            .pipe(map(this.extractData), catchError(this.serviceError));
-    }
+  cancelVisit(visitId: string, dto: VisitCancelRequestDto): Observable<any> {
+    return this.httpClient
+      .patch(
+        `${this.url}app/announcement-visit/cancel-visit/${visitId}`,
+        dto,
+        this.authorizedHeader()
+      )
+      .pipe(map(this.extractData), catchError(this.serviceError));
+  }
 
-    cancelVisit(visitId: string, dto: VisitCancelRequestDto): Observable<any> {
-        return this.httpClient
-            .patch(`${this.url}app/announcement-visit/cancel-visit/${visitId}`, dto, this.authorizedHeader())
-            .pipe(map(this.extractData), catchError(this.serviceError));
-    }
+  removeVisit(visitId: string, dto: AnnouncementVisitRemoveRequestDto) {
+    return this.httpClient
+      .patch(
+        `${this.url}app/announcement-visit/update-status/${visitId}`,
+        dto,
+        this.authorizedHeader()
+      )
+      .pipe(map(this.extractData), catchError(this.serviceError));
+  }
 
-    removeVisit(visitId: string, dto: AnnouncementVisitRemoveRequestDto) {
-        return this.httpClient
-            .patch(`${this.url}app/announcement-visit/update-status/${visitId}`, dto, this.authorizedHeader())
-            .pipe(map(this.extractData), catchError(this.serviceError));
-    }
+  getById(_id: string) {
+    return this.httpClient
+      .get(
+        `${this.url}app/announcement-visit/id/${_id}`,
+        this.anonymousHeader()
+      )
+      .pipe(map(this.extractData), catchError(this.serviceError));
+  }
 
-    getById(_id: string) {
-        return this.httpClient.get(`${this.url}app/announcement-visit/id/${_id}`, this.anonymousHeader())
-            .pipe(map(this.extractData), catchError(this.serviceError));
-    }
+  registerCompanion(dto: CompanionRegisterRequestDto, code:number) {
+    return this.httpClient
+      .post(
+        `${this.url}app/announcement-visit/register-companion-visit/${code}`,
+        dto,
+        this.anonymousHeader()
+      )
+      .pipe(map(this.extractData), catchError(this.serviceError));
+  }
 
-    registerCompanion(dto: CompanionRegisterRequestDto) {
-        return this.httpClient
-        .post(`${this.url}app/announcement-visit/register-companion-visit`, dto, this.anonymousHeader())
-        .pipe(map(this.extractData), catchError(this.serviceError));
-    }
-
+  sendCode(dto: { phone: string }) {
+    return this.httpClient
+      .post(`${this.url}app/announcement-visit/send-code`, dto)
+      .pipe(map(this.extractData), catchError(this.serviceError));
+  }
 }
